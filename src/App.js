ff988ce01a23c3cf17ac43a2b8816ee6f5db8dd9
Brittany Barden <bbarden93@yahoo.com>
2018-03-29 10:19:17 -0700

    add array and map of array

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import './App.css';
 class App extends Component {
 
   state = {
-    
+    signups: [
+      {name: "A", email: "a@gmail.com"},
+      {name: "B", email: "b@gmail.com"},
+      {name: "C", email: "c@gmail.com"}
+    ]
   }
 
   render() {
+    const {signups} = this.state
     return (
       <div className="App">
-        
+        <ul>
+          {signups.map((person, index) => {
+            return (
+              <li key={index}>{person.name}</li>
+            )
+          })}
+        </ul>
       </div>
     );
   }
